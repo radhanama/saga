7b18c02d8d9ad6df898e9ecc598c33289570f860
radhanama <radhanama@hotmail.com>
2025-06-23 20:38:43 +0000

    fixes

--- backend/Migrations/20250623203025_AddCamposUsuario.Designer.cs
+++ backend/Migrations/20250623203025_AddCamposUsuario.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using saga.Infrastructure.Repositories;
@@ -11,9 +12,11 @@ using saga.Infrastructure.Repositories;
 namespace saga.Migrations
 {
     [DbContext(typeof(ContexRepository))]
-    partial class ContexRepositoryModelSnapshot : ModelSnapshot
+    [Migration("20250623203025_AddCamposUsuario")]
+    partial class AddCamposUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
