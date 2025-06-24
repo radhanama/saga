7d96885146b280aafa92dc798fb99b96caae74fa
Radhanama <radhanama@hotmail.com>
2025-06-23 22:07:52 -0300

    Atualiza sidebar e tela inicial

--- front/src/components/Sidebar.jsx
+++ front/src/components/Sidebar.jsx
@@ -20,28 +20,76 @@ export default function Sidebar({ open, onNavigate }) {
       </div>
       <NavLink to="/" onClick={onNavigate}>Início</NavLink>
       {(role === 'Professor' || role === 'Administrator') && (
-        <NavLink to="/students" onClick={onNavigate}>Estudantes</NavLink>
+        <NavLink to="/students" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/student.png'} alt="Estudantes" />
+          Estudantes
+        </NavLink>
       )}
       {role === 'Student' && studentId && (
-        <NavLink to={`/students/${studentId}`} onClick={onNavigate}>Meu Perfil</NavLink>
+        <NavLink to={`/students/${studentId}`} onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/student.png'} alt="Perfil" />
+          Meu Perfil
+        </NavLink>
+      )}
+      {role === 'Student' && (
+        <NavLink to="/extensions" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/calender.png'} alt="Extensões" />
+          Pedidos de Extensão
+        </NavLink>
       )}
       {role === 'Administrator' && (
-        <NavLink to="/professors" onClick={onNavigate}>Professores</NavLink>
+        <NavLink to="/professors" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/professor.png'} alt="Professores" />
+          Professores
+        </NavLink>
       )}
       {role === 'Administrator' && (
-        <NavLink to="/researchers" onClick={onNavigate}>Pesquisadores</NavLink>
+        <NavLink to="/researchers" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/researcher.png'} alt="Pesquisadores" />
+          Pesquisadores
+        </NavLink>
       )}
       {(role === 'Administrator' || role === 'Professor') && (
-        <NavLink to="/researches" onClick={onNavigate}>Dissertações</NavLink>
+        <NavLink to="/researches" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/research.png'} alt="Dissertações" />
+          Dissertações
+        </NavLink>
       )}
       {(role === 'Administrator' || role === 'Professor') && (
-        <NavLink to="/projects" onClick={onNavigate}>Projetos</NavLink>
+        <NavLink to="/projects" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/lamp.png'} alt="Projetos" />
+          Projetos
+        </NavLink>
+      )}
+      {(role === 'Administrator' || role === 'Professor' || role === 'Student') && (
+        <NavLink to="/courses" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/lamp.png'} alt="Cursos" />
+          Cursos
+        </NavLink>
+      )}
+      {(role === 'Administrator' || role === 'Professor' || role === 'Student') && (
+        <NavLink to="/researchLines" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/research.png'} alt="Linhas" />
+          Linhas de Pesquisa
+        </NavLink>
       )}
       {role === 'Administrator' && (
-        <NavLink to="/extensions" onClick={onNavigate}>Extensões</NavLink>
+        <NavLink to="/users" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/professor.png'} alt="Usuários" />
+          Usuários
+        </NavLink>
       )}
       {role === 'Administrator' && (
-        <NavLink to="/entities/csv" onClick={onNavigate}>Carregar CSV</NavLink>
+        <NavLink to="/extensions" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/calender.png'} alt="Extensões" />
+          Extensões
+        </NavLink>
+      )}
+      {role === 'Administrator' && (
+        <NavLink to="/entities/csv" onClick={onNavigate}>
+          <img src={process.env.PUBLIC_URL + '/csv3.png'} alt="Carregar CSV" />
+          Carregar CSV
+        </NavLink>
       )}
     </nav>
   );
